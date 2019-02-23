90d4115a0899ba41775e1f950110d8860c7c5a09
johnehudsonjr <jeh.hudson@hotmail.com>
2019-02-22 19:59:04 -0500

    mongoDB setup

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
+
 const app = express();
 
-// DB Config
-const db = require('./config/keys').mongoURI
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+// mongoose
+//   .connect(db)
+//   .then(() => console.log('MongoDB Connected'))
+//   .catch(err => console.log(err));
+
+mongoose
+   .connect(db, { useNewUrlParser: true })
+   .then(() => console.log("MongoDB is connected"))
+   .catch(err => console.log(err));
+
 
 app.get('/', (req, res)=> res.send('Hello John, this is  from your server!'));
 
