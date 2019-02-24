725df9ec27f5845e96792a657312ff7435e041db
johnehudsonjr <jeh.hudson@hotmail.com>
2019-02-24 10:28:02 -0500

    git ignore

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users')
+const profile = require('./routes/api/profile')
+const posts = require('./routes/api/posts')
 
 const app = express();
 
@@ -15,6 +18,11 @@ mongoose
 
 app.get('/', (req, res)=> res.send('Hello John, this is  from your server!'));
 
+// use routes EX. When a user types in this url (___/api/users) it will go to to the users page in api folder and run what is called in that folder.
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 // Create a Port to run it on heroku or local host 5000
 const port = process.env.PORT || 5000;
