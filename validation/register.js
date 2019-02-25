f2201b5dc1b9c843f1149cd85c5afba69d1cc5aa
johnehudsonjr <jeh.hudson@hotmail.com>
2019-02-25 18:14:52 -0500

    validation

--- validation/register.js
+++ validation/register.js
@@ -6,8 +6,19 @@
    module.exports = function validateRegisterInput(data){
       let errors = {};
 
+      // Terenary that test if it is an empty string or not.
+      // If it !isEmpty (is not empty)use its value. If it is an empty string, it will follow the if statement below
+      data.name  = !isEmpty(data.name) ? data.name : '';
+      data.email  = !isEmpty(data.email) ? data.email : '';
+      data.password = !isEmpty(data.password) ? data.password : '';
+      data.password2  = !isEmpty(data.password2) ? data.password2 : '';
+
       if(!Validator.isLength(data.name, {min:2, max:30})){
          errors.name = 'Name must be between 2 and 30 characters'
+      }
+
+      if(Validator.isEmpty(data.name)){
+         errors.name = 'Name field is required';
       }
 
       return{
