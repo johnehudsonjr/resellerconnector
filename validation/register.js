46bd93988736dddecdc5b52c537384893e8c5d6a
johnehudsonjr <jeh.hudson@hotmail.com>
2019-02-25 18:27:33 -0500

    validations strings

--- validation/register.js
+++ validation/register.js
@@ -17,9 +17,34 @@
          errors.name = 'Name must be between 2 and 30 characters'
       }
 
-      if(Validator.isEmpty(data.name)){
-         errors.name = 'Name field is required';
+      // Empty string validations.
+
+      if(Validator.isEmpty(data.email)){
+         errors.email = 'Email field is required';
       }
+
+      if(Validator.isEmail(data.email)){
+         errors.email = 'Email is invalid.';
+      }
+
+      if(Validator.isEmpty(data.password)){
+         errors.password = 'Password field is required';
+      }
+
+      if(!Validator.isLength(data.password, {min: 6, max:30})){
+         errors.password = 'Password must be at least 6 characters.'
+      }
+
+      if(Validator.isEmpty(data.password2)){
+         errors.password = 'Confirm password field is required';
+      }
+
+      if(Validator.equals(data.password, data.password2)){
+         errors.password2 = 'Passwords must match';
+      }
+     
+
+
 
       return{
          errors, 
