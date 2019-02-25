ae9611db8c76c33382b7ff99db3ab01fe9201ca7
johnehudsonjr <jeh.hudson@hotmail.com>
2019-02-24 21:50:26 -0500

    Json token auth

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 // module.exports makes object available outside of this file.
 module.exports = {
-   mongoURI: 'mongodb://john123:john123@resellerconnector-shard-00-00-txaq4.mongodb.net:27017,resellerconnector-shard-00-01-txaq4.mongodb.net:27017,resellerconnector-shard-00-02-txaq4.mongodb.net:27017/test?ssl=true&replicaSet=resellerconnector-shard-0&authSource=admin&retryWrites=true'
+   mongoURI: 'mongodb://john123:john123@resellerconnector-shard-00-00-txaq4.mongodb.net:27017,resellerconnector-shard-00-01-txaq4.mongodb.net:27017,resellerconnector-shard-00-02-txaq4.mongodb.net:27017/test?ssl=true&replicaSet=resellerconnector-shard-0&authSource=admin&retryWrites=true',
+   secretOrKey: 'secret'
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // This is used to encrypt the password
 const bcrypt = require('bcryptjs')
@@ -74,8 +76,26 @@ router.post('/login', (req,res) => {
       bcrypt.compare(password, user.password)
       .then(isMatch =>{
          if(isMatch){
-            res.json({msg:'success'});
+         //   User matched
+
+         // createPayload
+         const payload = {  id: user.id, name: user.name, avatar:user.avatar} 
+         
+         // Sign Token
+         jwt.sign(payload,
+            keys.secretOrKey, 
+            {expiresIn: 3600},
+            (err, token) =>{
+               res.json({
+                  success: true,
+                  token: 'Bearer ' + token
+               })
+            }
+            
+            );
+
          }
+
          else{
             return res.status(400).json({password:'Password Incorrect'});
          }
