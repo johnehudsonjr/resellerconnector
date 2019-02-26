fc75e120e4e260cccd7bd14611a294f0a31636bc
johnehudsonjr <jeh.hudson@hotmail.com>
2019-02-25 19:23:44 -0500

    login validation

--- validation/login.js
+++ validation/login.js
@@ -1,2 +1,33 @@
-// rules for login
-// 
+// rules for registrations
+
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateLoginInput(data){
+   let errors = {};
+
+   // Terenary that test if it is an empty string or not.
+   // If it !isEmpty (is not empty)use its value. If it is an empty string, it will follow the if statement below
+
+   data.email  = !isEmpty(data.email) ? data.email : '';
+   data.password = !isEmpty(data.password) ? data.password : '';
+
+
+  
+   if(Validator.isEmpty(data.email)){
+      errors.email = 'Email field is required';
+   }
+
+   if(!Validator.isEmail(data.email)){
+      errors.email = 'Email is invalid.';
+   }
+
+   if(Validator.isEmpty(data.password)){
+      errors.password = 'Password field is required';
+   }
+
+   return{
+      errors, 
+      isValid: isEmpty(errors)
+   }
+}
