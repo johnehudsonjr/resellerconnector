f647d5a50a53ac0ce4e5e83488103d3fdb22c1a3
johnehudsonjr <jeh.hudson@hotmail.com>
2019-02-26 18:50:15 -0500

    post model

--- models/Post.js
+++ models/Post.js
@@ -29,6 +29,31 @@ const PostSchema = new Schema({
       }
    ],
    comments: [
-      
-   ]
+      {
+         user: {
+            type: Schema.Types.ObjectId,
+            ref:'users'
+         },
+         text:{
+            type: String,
+            required: true
+         },
+         name: {
+            type: String
+         },
+         avatar: {
+            type: String
+         },
+         date:{
+            type: Date,
+            default: Date.now
+         }
+      }
+   ],
+   date:{
+      type: Date,
+      default: Date.now
+   }
 });
+
+module.exports = Post = mongoose.model('post', PostSchema)
