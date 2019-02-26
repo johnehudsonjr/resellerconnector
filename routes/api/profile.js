c0d17f979bf51e8cb91310cc05f207118ee22e40
johnehudsonjr <jeh.hudson@hotmail.com>
2019-02-26 15:09:35 -0500

    profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+// Load validation
+const validateProfileInput = require('../../validation/profile')
+
 // Bring in profile model
 const Profile = require('../../models/Profile');
 // Bring in user model
@@ -13,13 +16,16 @@ const User = require('../../models/User');
 // @access  Public
 router.get('/test', (req, res) => res.json({msg: "Profile Works"})
 );
+
 // @route   GET api/profile/
 // @desc    Get current users profile
 // @access  Private
 
 router.get('/', passport.authenticate('jwt', {session:false}), (req, res) =>{
    const errors = {};
+
    Profile.findOne({user:req.user.id})
+   .populate('user', ['name', 'avatar'])
    .then(profile => {
       if(!profile){
          errors.noprofile = 'There is no profile for this user.'
@@ -30,11 +36,38 @@ router.get('/', passport.authenticate('jwt', {session:false}), (req, res) =>{
    .catch(err => res.status(404).json(err));
 });
 
+// @route   GET api/profile/handle/:handle (BACKEND API ROUTE)
+// @desc    GET PROFILE BY HANDLE
+// @access  Public: Anyone can see a profile
+
+router.get('/handle/:handle', (req, res) => {
+   const errors = {};
+   // params.handle will match with the :handle in url(in database)
+   Profile.findOne({handle:req.params.handle})
+   .populate('user',['name', 'avatar'])
+   .then(profile => {
+      if(!profile){
+         errors.noprofile = "There is no profile for this user";
+         res.status(404).json(errors);
+      }
+      res.json(profile)
+   })
+   .catch(err => res.status(404).json(err))
+});
+
+
 // @route   POST api/profile/
 // @desc   Create or edit user profile
 // @access  Private
 
 router.post('/', passport.authenticate('jwt', {session:false}), (req, res) =>{
+
+   const{errors, isValid} = validateProfileInput(req.body);
+   // Check Validation
+   if(!isValid){
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+   }
 
    // Get Fields: This is what will be the form that gets filled
    const profileFields = {};
--- validation/profile.js
+++ validation/profile.js
@@ -17,15 +17,15 @@ module.exports = function validateProfileInput(data){
       errors.handle = 'Handle needs to be between 2 and 40 characters.';
    }
 
-   if(!Validator.isEmpty(data.handle)){
+   if(Validator.isEmpty(data.handle)){
       errors.handle = 'Profile handle is required.';
    }
 
-   if(!Validator.isEmpty(data.status)){
+   if(Validator.isEmpty(data.status)){
       errors.status = 'Status field is required.';
    }
 
-   if(!Validator.isEmpty(data.status)){
+   if(Validator.isEmpty(data.status)){
       errors.skills = 'Skills field is required.';
    }
 
@@ -34,24 +34,29 @@ module.exports = function validateProfileInput(data){
          errors.website = 'Not a valid URL'
       }
    }
-   if(!isEmpty(data.website)){
-      if(!Validator.isURL(data.website)){
-         errors.website = 'Not a valid URL'
+   if(!isEmpty(data.youtube)){
+      if(!Validator.isURL(data.youtube)){
+         errors.youtube = 'Not a valid URL'
       }
    }
-   if(!isEmpty(data.website)){
-      if(!Validator.isURL(data.website)){
-         errors.website = 'Not a valid URL'
+   if(!isEmpty(data.twitter)){
+      if(!Validator.isURL(data.twitter)){
+         errors.twitter = 'Not a valid URL'
       }
    }
-   if(!isEmpty(data.website)){
-      if(!Validator.isURL(data.website)){
-         errors.website = 'Not a valid URL'
+   if(!isEmpty(data.facebook)){
+      if(!Validator.isURL(data.facebook)){
+         errors.facebook = 'Not a valid URL'
       }
    }
-   if(!isEmpty(data.website)){
-      if(!Validator.isURL(data.website)){
-         errors.website = 'Not a valid URL'
+   if(!isEmpty(data.linkedin)){
+      if(!Validator.isURL(data.linkedin)){
+         errors.linkedin = 'Not a valid URL'
+      }
+   }
+   if(!isEmpty(data.instagram)){
+      if(!Validator.isURL(data.instagram)){
+         errors.instagram = 'Not a valid URL'
       }
    }
 
