f1607ecf7fd2692921bc4d9d5de4ccdad725fbb6
johnehudsonjr <jeh.hudson@hotmail.com>
2019-02-26 18:06:47 -0500

    router delete

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -229,4 +229,25 @@ router.post('/education', passport.authenticate('jwt', {session:false}),(req,res
     })
 });
 
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    delete experience
+// @access  Private
+
+router.delete('/experience/:exp_id', passport.authenticate('jwt', {session:false}),(req,res) => {
+
+   
+    Profile.findOne({user: req.user.id})
+      .then(profile =>{
+         // Get remove index
+         const removeIndex = profile.experience
+         .map(item => item.id)
+         .indexOf(req.params.exp_id);
+
+         // splice out of array
+         profile.experience.splice(removeIndex, 1);
+         profile.save().then(profile => res.json(profile))
+    })
+    .catch(err => res.status(404).json(err));
+});
+
 module.exports = router;
