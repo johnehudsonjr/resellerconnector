ce2885e434946832c245ba0cf9f8084f2d7282d1
johnehudsonjr <jeh.hudson@hotmail.com>
2019-02-26 14:05:32 -0500

    validators

---
+++ validation/profile.js
@@ -0,0 +1,64 @@
+// rules for registrations
+
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateProfileInput(data){
+   let errors = {};
+
+   // Terenary that test if it is an empty string or not.
+   // If it !isEmpty (is not empty)use its value. If it is an empty string, it will follow the if statement below
+
+   data.handle  = !isEmpty(data.handle) ? data.handle : '';
+   data.status = !isEmpty(data.status) ? data.status : '';
+   data.skills = !isEmpty(data.skills) ? data.skills : '';
+
+   if(!Validator.isLength(data.handle, {min: 2, max:40})){
+      errors.handle = 'Handle needs to be between 2 and 40 characters.';
+   }
+
+   if(!Validator.isEmpty(data.handle)){
+      errors.handle = 'Profile handle is required.';
+   }
+
+   if(!Validator.isEmpty(data.status)){
+      errors.status = 'Status field is required.';
+   }
+
+   if(!Validator.isEmpty(data.status)){
+      errors.skills = 'Skills field is required.';
+   }
+
+   if(!isEmpty(data.website)){
+      if(!Validator.isURL(data.website)){
+         errors.website = 'Not a valid URL'
+      }
+   }
+   if(!isEmpty(data.website)){
+      if(!Validator.isURL(data.website)){
+         errors.website = 'Not a valid URL'
+      }
+   }
+   if(!isEmpty(data.website)){
+      if(!Validator.isURL(data.website)){
+         errors.website = 'Not a valid URL'
+      }
+   }
+   if(!isEmpty(data.website)){
+      if(!Validator.isURL(data.website)){
+         errors.website = 'Not a valid URL'
+      }
+   }
+   if(!isEmpty(data.website)){
+      if(!Validator.isURL(data.website)){
+         errors.website = 'Not a valid URL'
+      }
+   }
+
+   return{
+      errors,
+      isValid: isEmpty(errors)
+   }
+     
+  
+}
