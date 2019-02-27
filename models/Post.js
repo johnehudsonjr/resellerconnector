6a9c3427f7114c91a091e953baac7c0a52c69153
johnehudsonjr <jeh.hudson@hotmail.com>
2019-02-27 10:18:38 -0500

    react app

--- models/Post.js
+++ models/Post.js
@@ -20,41 +20,41 @@ const PostSchema = new Schema({
    avatar: {
       type: String
    },
-   
-   // Array of user likes
-   likes: [
-      {
-         user: {
-            type: Schema.Types.ObjectId,
-            ref:'users'
 
-         }
-      }
-   ],
+   // Array of user likes
+   // likes: [
+   //    {
+   //       user: {
+   //          type: Schema.Types.ObjectId,
+   //          ref:'users'
+
+   //       }
+   //    }
+   // ],
 
    // Array of user comments
-   comments: [
-      {
-         user: {
-            type: Schema.Types.ObjectId,
-            ref:'users'
-         },
-         text:{
-            type: String,
-            required: true
-         },
-         name: {
-            type: String
-         },
-         avatar: {
-            type: String
-         },
-         date:{
-            type: Date,
-            default: Date.now
-         }
-      }
-   ],
+   // comments: [
+   //    {
+   //       user: {
+   //          type: Schema.Types.ObjectId,
+   //          ref:'users'
+   //       },
+   //       text:{
+   //          type: String,
+   //          required: true
+   //       },
+   //       name: {
+   //          type: String
+   //       },
+   //       avatar: {
+   //          type: String
+   //       },
+   //       date:{
+   //          type: Date,
+   //          default: Date.now
+   //       }
+   //    }
+   // ],
    // Post Date
    date:{
       type: Date,
