f000f72271467eaacb4d2793a2e676946066eb93
johnehudsonjr <jeh.hudson@hotmail.com>
2019-02-27 18:21:51 -0500

    bootstrap navbar and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
 
 import './App.css';
 
@@ -6,7 +9,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <Navbar/>
         <h1>John's REACT APP</h1>
+        <Footer/>
       </div>
     );
   }
