d399ddf675d48efa902315b5579cac1a7750cdf3
johnehudsonjr <jeh.hudson@hotmail.com>
2019-02-27 19:00:14 -0500

    css and react router

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -3,11 +3,23 @@ import React, { Component } from 'react'
 class Landing extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="landing">
+      <div className="dark-overlay landing-inner text-light">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 text-center">
+              <h1 className="display-3 mb-4">Reseller Connector
+              </h1>
+              <p className="lead"> Create your reseller profile and connect with other resellers!</p>
+              <hr />
+              <a href="register.html" className="btn btn-lg btn-info mr-2">Sign Up</a>
+              <a href="login.html" className="btn btn-lg btn-light">Login</a>
+            </div>
+          </div>
+        </div>
       </div>
-    )
-  }
+    </div>
+   )
 }
-
+}
 export default Landing;
